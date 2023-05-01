ae6609d1d094f7eb57285f98ad70297f90a6e076
Ali Yeysides <ali.yeysides@gmail.com>
2023-04-30 20:18:10 -0400

    adds net/html package and dfs func

--- parse.go
+++ parse.go
@@ -1,6 +1,9 @@
 package link
 
-import "io"
+import (
+	"io"
+	"golang.org/x/net/html"
+)
 
 // Link represents a link (<a href="...">) in an HTML document.
 type Link struct {
@@ -10,5 +13,19 @@ type Link struct {
 
 // Parse will take in an HTML document and will return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+  doc, err := html.Parse(r)
+  if err != nil {
+    return nil, err
+  }
+  dfs(doc)
   return nil, nil
+}
+
+func dfs(n *html.Node) {
+  if n.Type == html.ElementNode && n.Data == "a" {
+    println(n.Data)
+  }
+  for c := n.FirstChild; c != nil; c = c.NextSibling {
+    dfs(c)
+  }
 }
