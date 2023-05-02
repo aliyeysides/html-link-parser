1111001c83cfd4049e399d55e019d3e9e2d2aaa9
Ali Yeysides <ali.yeysides@gmail.com>
2023-05-01 23:45:16 -0400

    format

--- parse.go
+++ parse.go
@@ -2,37 +2,50 @@ package link
 
 import (
 	"fmt"
-	"io"
 	"golang.org/x/net/html"
+	"io"
 )
 
 // Link represents a link (<a href="...">) in an HTML document.
 type Link struct {
-  Href string
-  Text string
+	Href string
+	Text string
 }
 
 // Parse will take in an HTML document and will return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
-  doc, err := html.Parse(r)
-  if err != nil {
-    return nil, err
-  }
-  nodes := linkNodes(doc)
-  for _, node := range nodes {
-    fmt.Println(node)
-  }
-  return nil, nil
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+		fmt.Println(node)
+	}
+	return nil, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, a := range n.Attr {
+		if a.Key == "href" {
+			ret.Href = a.Val
+			break
+		}
+	}
+	ret.Text = "need to finish this"
+	return ret
 }
 
 func linkNodes(n *html.Node) []*html.Node {
-  var ret []*html.Node
-  if n.Type == html.ElementNode && n.Data == "a" {
-    ret = append(ret, n)
-  }
-  for c := n.FirstChild; c != nil; c = c.NextSibling {
-    ret = append(ret, linkNodes(c)...)
-  }
-  return ret
+	var ret []*html.Node
+	if n.Type == html.ElementNode && n.Data == "a" {
+		ret = append(ret, n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
-
